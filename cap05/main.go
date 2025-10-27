fdb51995f0d5ad0db41be9e0f97c8d7ed3ab4530
joel-briones-B-Low <joellbriones701@gmail.com>
2025-10-27 11:21:23 -0600

    add buffer capacity

--- cap05/main.go
+++ cap05/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func tarea(id int, canal chan<- string) {
+	time.Sleep(time.Duration(id) * 1000 * time.Millisecond)
+	canal <- fmt.Sprintf("Tarea %d completada", id)
+}
+
 func main() {
+
+	canal := make(chan string, 3)
+
+	for i := range 6 {
+		go tarea(i, canal)
+
+	}
+
+	timeout := time.After(4 * time.Second)
+
+	for range 6 {
+		select {
+		case msg := <-canal:
+			fmt.Println(msg)
+		case <-timeout:
+			fmt.Println("Tiempo de espera agotado")
+			return
+		}
+	}
 
 }
